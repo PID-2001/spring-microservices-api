f9bc20da1fdb43f0a78436508e038982848d4013
Pathum Isuru <pathumisuru19@gamil.com>
2026-02-25 00:50:58 +0530

    first commit

---
+++ eureka-server/src/test/java/com/pathum/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pathum.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
